a94dce47b047a43e91c6c82b86236cbfcdb638f1
Yasuharu Iida <iiharu@alumni.tus.ac.jp>
2025-12-31 21:23:01 +0900

    Other users (#16)
    * [WIP]: using symbolic link to usable for other user
    
    * fix: test

--- test/antigravity/test.sh
+++ test/antigravity/test.sh
@@ -26,7 +26,7 @@
 #    HOME=$(pwd)/test/antigravity/fixtures devcontainer features test \
 #                   --features antigravity \
 #                   --remote-user root \
-#                   --skip-scenarios   \
+#                   --skip-scenarios \
 #                   --base-image mcr.microsoft.com/devcontainers/base:ubuntu \
 #                   /path/to/this/repo
 
@@ -40,9 +40,12 @@ source dev-container-features-test-lib
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib. Syntax is...
 # check <LABEL> <cmd> [args...]
-check ".gitconfig is mounted" test -f /home/vscode/.gitconfig
-check ".gemini is mounted" test -d /home/vscode/.gemini
-check ".gemini/GEMINI.md is mounted" test -f /home/vscode/.gemini/GEMINI.md
+check ".gitconfig is mounted" test -f /var/tmp/.gitconfig
+check ".gitconfig is link" test -h $HOME/.gitconfig
+check ".gitconfig is linked" test "$(readlink $HOME/.gitconfig)" == "/var/tmp/.gitconfig"
+check ".gemini is mounted" test -d /var/tmp/.gemini
+check ".gemini is linked" test -h $HOME/.gemini
+check ".gitconfig is linked" test "$(readlink $HOME/.gemini)" == "/var/tmp/.gemini"
 check ".bash_aliases is created" test -f $HOME/.bash_aliases
 
 # Report results
