07a6f71e42203e2d8248f97150b639ec2db4408f
iiharu <iiharu@alumni.tus.ac.jp>
2025-12-31 03:45:32 +0000

    feat: add code shims

--- src/antigravity/install.sh
+++ src/antigravity/install.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
+
 set -e
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+FEATURE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# override shim, it redirects to antigravity
+cp -f "${FEATURE_DIR}/bin/code" /usr/local/bin/
+chmod +rx /usr/local/bin/code
+
+exit $?
