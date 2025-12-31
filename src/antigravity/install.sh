15229dea70c7d0b8b2c30964b2cc168f76647888
iiharu <iiharu@alumni.tus.ac.jp>
2025-12-31 05:33:22 +0000

    feat: install antigravty alias

--- src/antigravity/install.sh
+++ src/antigravity/install.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
+
 set -e
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+ZSH_CUSTOM="${_REMOTE_USER_HOME}/.oh-my-zsh/custom"
+
+cat << 'EOF' > "${ZSH_CUSTOM}/antigravity.zsh"
+alias code='command antigravity'
+EOF
+
+chown -R ${_REMOTE_USER}:${_REMOTE_USER} "${ZSH_CUSTOM}/antigravity.zsh"
